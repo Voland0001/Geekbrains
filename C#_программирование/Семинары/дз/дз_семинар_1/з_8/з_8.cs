af802ed123bc576344ff2a302546aec9fb41ea32
Dubok <dlyapokupokspb@yandex.ru>
2022-08-28 11:10:04 +0300

    Дз по семинару 2 с условиями

--- C#_программирование/Семинары/дз/дз_семинар_1/з_8/з_8.cs
+++ C#_программирование/Семинары/дз/дз_семинар_1/з_8/з_8.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine());
 int current = 2;
-while ((current < a) && (current % 2 == 0))
+while (current < a) 
 {
     Console.Write($"{current } ");
     current+=2;
