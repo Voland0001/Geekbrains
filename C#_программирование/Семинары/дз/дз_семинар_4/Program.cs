2ee88996d291cf733c203939be392d99b6b42a79
Dubok <dlyapokupokspb@yandex.ru>
2022-09-01 17:50:38 +0300

    Дз по семинару 3 с условиями

--- C#_программирование/Семинары/дз/дз_семинар_4/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int A = int.Parse(Console.ReadLine()); 
-Console.WriteLine("Hello, World!");
-int B = int.Parse(Console.ReadLine()); 
