62da23674ecd92706bb8462c48a9abf677dfd0fd
Dubok <dlyapokupokspb@yandex.ru>
2022-09-01 17:19:02 +0300

    Дз семинар 3

---
+++ C#_программирование/Семинары/дз/дз_семинар_3/дз_21/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine("Введите координаты x1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты z1: ");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты x2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты y2: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты z2: ");
+int z2 = int.Parse(Console.ReadLine());
+int ax = (x1 - x2) * (x1 - x2);
+int ay = (y1 - y2) * (y1 - y2);
+int az = (z1 - z2) * (z1 - z2);
+int sum = ax + ay + az;
+double result = Math.Sqrt(sum);
+Console.WriteLine($"Длина вектора равна {Math.Round(result,2)}");
+
+
+
+
+
+
+
+
+
+
---
+++ C#_программирование/Семинары/дз/дз_семинар_4/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int A = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Hello, World!");
+int B = int.Parse(Console.ReadLine()); 
