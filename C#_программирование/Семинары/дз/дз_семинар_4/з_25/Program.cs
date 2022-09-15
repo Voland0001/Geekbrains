bdab3073658a337ea79c19d4947697945488f08f
Dubok <dlyapokupokspb@yandex.ru>
2022-09-15 17:04:05 +0300

    Дз к семинару 4

---
+++ C#_программирование/Семинары/дз/дз_семинар_4/з_25/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 25: 
+// Напишите цикл, который принимает 
+// на вход два числа (A и B) и возводит число A 
+// в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void Ansver (int num1, int num2)
+{
+    int index = 0;
+    int result = 1;
+    while (index <= num2)
+    {
+        if (index == num2)
+        { 
+        Console.WriteLine($"A в степени B = {result}"); 
+        break;       
+        }
+        else if (index < num2)
+        {
+        result = result * num1;
+        index++;
+        }
+    }      
+}
+
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+Ansver(A, B);
---
+++ C#_программирование/Семинары/дз/дз_семинар_4/з_29/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов, 
+// заполненных случайнми числами из [a, b) и выводит их на экран.
+// 5, 0, 20 -> [1, 2, 5, 7, 19]
+// 3, 1, 35 -> [6, 1, 33]
+
+void Newmas(int[] col, int num1, int num2)
+{
+    int length = col.Length;
+    int index = 0;
+    while(index<length)
+    {
+        if (num1<=num2)
+        {col[index]= new Random().Next(num1, num2);
+        }
+        else if (num1>num2)
+        {col[index]= new Random().Next(num2, num1);
+        }
+        index++;
+    }
+}  
+void Printmas(int[] collection)
+{
+    int len = collection.Length;
+    int count = 0;
+    while(count<len)
+    {
+        Console.Write($"{collection[count]}, ");
+        count++;
+    }    
+}
+
+Console.WriteLine("Введите количество чисел массива: ");
+int n =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число начала диапазона массива: ");
+int number1 =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число конца диапазона массива: ");
+int number2 =int.Parse(Console.ReadLine());
+int[] newarray = new int[n];
+
+Newmas(newarray, number1, number2);
+Printmas(newarray);
+
+
