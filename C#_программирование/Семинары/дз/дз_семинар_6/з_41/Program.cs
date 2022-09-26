00ab06c033dde075b72acfc7777af1a3028b5463
Dubok <dlyapokupokspb@yandex.ru>
2022-09-26 19:55:53 +0300

    Дз семинар 6

---
+++ C#_программирование/Семинары/дз/дз_семинар_6/з_41/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void Num (int[] mass, int length)
+{
+    int i = 0;
+    while (i<length)
+    {
+        Console.WriteLine("Введите число эллемента массива: ");
+        mass[i] = int.Parse(Console.ReadLine());
+        i++;
+    }
+}
+
+
+void Print(int[] collection)
+{Console.WriteLine($"Все элементы массива: {String.Join(", ", collection)}");
+} 
+
+
+void Res (int[] arr)
+{
+    int length = arr.Length;
+    int i = 0;
+    int count = 0;
+    while (i<length)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+            i++;
+        }
+        else 
+        {
+            i++;
+        }
+    }
+    Console.Write($"Числа больше нуля: {count}");
+}
+
+
+Console.WriteLine("Введите количество чисел массива: ");
+int n =int.Parse(Console.ReadLine());
+int[] result = new int[n];
+Num(result, n);
+Print(result);
+Res(result);
---
+++ C#_программирование/Семинары/дз/дз_семинар_6/з_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт 
+// точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите чисело b1: ");
+double b1 =double.Parse(Console.ReadLine());
+Console.WriteLine("Введите чисело b2: ");
+double b2 =double.Parse(Console.ReadLine());
+Console.WriteLine("Введите чисело k1: ");
+double k1 =double.Parse(Console.ReadLine());
+Console.WriteLine("Введите чисело k2: ");
+double k2 =double.Parse(Console.ReadLine());
+
+void Result(double numb1, double numb2, double numk1, double numk2)
+{
+    double arg = (numb2-numb1)/(numk1-numk2);
+    double fun = numk1 * (arg) + numb1;
+    Console.WriteLine($"({arg}, {fun})");
+}
+
+Result(b1, b2, k1, k2); 
